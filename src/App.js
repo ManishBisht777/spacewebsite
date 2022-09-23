aa43c5733ce2ebbc50002f978b8f605e9ee1e768
manish <Manishbisht9711@gmail.com>
2022-09-23 13:52:07 +0530

    clean up and project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius laborum
+        quibusdam consequuntur ratione tempore obcaecati a voluptates incidunt
+        neque maiores necessitatibus eos provident, magni, enim eaque culpa?
+        Dolor, possimus explicabo.
+      </p>
     </div>
   );
 }

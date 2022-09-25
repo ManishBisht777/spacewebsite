c963dad53a95101217349b621758bbfe96faac2f
manish <Manishbisht9711@gmail.com>
2022-09-25 18:42:12 +0530

    navbar responsive

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/shared/logo.svg";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 
+import openicon from "../../assets/shared/icon-hamburger.svg";
+import closeicon from "../../assets/shared/icon-close.svg";
+
 const Navbar = () => {
+  const [navopen, setnavopen] = useState(false);
   return (
     <nav>
       <div className="logo">
@@ -33,6 +37,54 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
+      <div className="mobilebar">
+        {!navopen ? (
+          <div
+            className="navicon"
+            onClick={() => {
+              setnavopen(!navopen);
+            }}
+          >
+            <img src={openicon} alt="" />
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+      {navopen && (
+        <>
+          <div
+            onClick={() => {
+              setnavopen(!navopen);
+            }}
+            className=" hamburger"
+          >
+            <img src={closeicon} className="closeicon" alt="" />
+            <ul>
+              <li>
+                <Link to="/">
+                  <span>01</span> Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/destination">
+                  <span>02</span> Destination
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <span>03</span> Crew
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <span>04</span> Technology
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </>
+      )}
     </nav>
   );
 };
